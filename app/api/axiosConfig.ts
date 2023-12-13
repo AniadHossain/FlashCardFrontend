cf9b01d2194143c07579e0fe00b34ca8d1598aa3
Aniad <aniad.hossain@outlook.com>
2023-12-13 04:03:24 +0000

    Update baseUrl for axios

--- app/api/axiosConfig.ts
+++ app/api/axiosConfig.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:8080",
+    baseURL:"http://35.178.153.32",
     headers:{
         "Content-type":"application/json"
     }
 })
 
 export const axiosAuth = axios.create({
-    baseURL:"http://localhost:8080",
+    baseURL:"http://35.178.153.32",
     headers:{
         "Content-type":"application/json"
     }
